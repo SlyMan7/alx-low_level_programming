b3978be1212952342ddae6a763bebcc929a776b6
SlyMan7 <mkagwari@gmail.com>
2023-09-14 04:27:26 -0700

    9timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,8 +3,8 @@
 
 /**
  *print_time_table - function to Prints the 9 times table, starting with 0
- *
- *Return: 0
+ *@param n The number to start the times table from
+ *Return void
  *
  */
 void times_table(void)
@@ -16,7 +16,7 @@ void times_table(void)
 		for (j = 0 ; j < 10 ; j++)
 		{
 			result = i * j;
-			if (j == 0) 
+			if (j == 0)
 				printf("%d,", result);
 			else
 			{
